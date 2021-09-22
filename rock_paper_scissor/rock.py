ba205ba0529680b8b457d9905c0c7e3905b1453e
pauline_btx <boiteux.pauline@gmail.com>
2021-09-22 12:21:54 +0200

    rock, paper, scissor game

---
+++ rock_paper_scissor/rock.py
@@ -0,0 +1,46 @@
+import random
+
+choices = ["Pierre", "Papier", "Ciseaux"]
+print("if you want to end write 'End'")
+computer = random.choice(choices)
+player = False
+cpu_score = 0
+player_score = 0
+
+while True: 
+    player = input("Pierre, Papier, Ciseaux?").capitalize()
+
+    if player == computer: 
+        print("Play Again")
+
+    elif player == "Pierre":
+        if computer == "Papier":
+            print("You loose")
+            cpu_score+=1
+        else:
+            print("You win")
+            player_score+=1
+
+
+    elif player == "Papier":
+        if computer == "Ciseaux": 
+            print("You loose")
+            cpu_score+=1
+        else: 
+            print("You win")
+            player_score+=1
+
+    
+    elif player == "Ciseau":
+        if computer == "Pierre":
+            print("You loose")
+            cpu_score+=1
+        else: 
+            print("You win")
+            player_score+=1   
+
+
+    elif player == "End":
+        print("Your score:",player_score)
+        print("Computer score", cpu_score)
+        break 
